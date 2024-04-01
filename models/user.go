2698f904765e149bc4196e5e315ded2db9d5bc45
amirhossein.lashkari <amirhosseinlashgari.al@gmail.com>
2024-04-01 04:18:52 +0330

    save hashed password of users, adding utility function for hashing password

--- models/user.go
+++ models/user.go
@@ -1,6 +1,9 @@
 package models
 
-import "github.com/amirhlashgari/event-booking/db"
+import (
+	"github.com/amirhlashgari/event-booking/db"
+	"github.com/amirhlashgari/event-booking/utils"
+)
 
 type User struct {
 	ID       int64
@@ -16,7 +19,12 @@ func (u User) Save() error {
 	}
 	defer stmt.Close()
 
-	result, err := stmt.Exec(u.Email, u.Password)
+	hashedPassword, err := utils.HashPassword(u.Password)
+	if err != nil {
+		return err
+	}
+
+	result, err := stmt.Exec(u.Email, hashedPassword)
 	if err != nil {
 		return err
 	}
